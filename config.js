a6375759ef5cf4497e2dab1b03b872447997ff21
STAR BOY <107465084+ST4R-BOY@users.noreply.github.com>
2023-05-18 06:08:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@Rangga_bot'
-  var sticker_author = 'Rangga'
+  var sticker_name = '@xdd test v.1.9'
+  var sticker_author = 'XD'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Rangga Botz'
+global.wm = '©testv.1.9'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
